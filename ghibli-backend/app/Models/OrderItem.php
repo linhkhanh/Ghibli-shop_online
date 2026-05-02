7cba8da57a847a76005d66a03bc4f68bf4249942
linhkhanh <khanhlinh220592@gmail.com>
2026-05-02 18:59:11 +0700

    add: soft delete for products table

--- ghibli-backend/app/Models/OrderItem.php
+++ ghibli-backend/app/Models/OrderItem.php
@@ -15,6 +15,6 @@ class OrderItem extends Model
 
     public function product(): BelongsTo
     {
-        return $this->belongsTo(Product::class);
+        return $this->belongsTo(Product::class)->withTrashed();
     }
 }
